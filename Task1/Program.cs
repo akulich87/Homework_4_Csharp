552cd9169823d519a25fb405dc44672501cab0ff
akulich87 <amgrupp_project@mail.ru>
2023-06-25 20:04:50 +0300

    Added Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -4,15 +4,24 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+int NaturalDegree(int numA, int numB)
+{
+  int result = 1;
+
+  for(int i = 1; i <= numB; i++ )
+  {
+    result = result * numA;
+  }
+  return result;
+}
+
 Console.Write("Введите первое число: ");
 int numA = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int numB = Convert.ToInt32(Console.ReadLine());
 
-while(numB > 0)
-{
-  Console.Write(Math.Pow(numA,numB));
-  break;
-}
+int naturalDegree = NaturalDegree(numA, numB);
+
+Console.Write(naturalDegree);
 
 
